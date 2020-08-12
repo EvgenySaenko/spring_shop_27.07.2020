ee022b6c7b9d0e25233b24913284969cfac9aac8
Evgeny Saenko <storn84@mail.ru>
2020-08-12 17:52:28 +0300

    create a project in the master branch

---
+++ src/main/java/ru/geekbrains/sample/persistence/entities/Book.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.sample.persistence.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Book extends AbstractEntity {
+    public String name;
+    public Date created;
+    public String description;
+    public boolean available;
+}
